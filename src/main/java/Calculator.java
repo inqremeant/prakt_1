7de9e0de9347d4130229b4c03e2a49b033bbffa8
inqremeant <zizinvana@gmail.com>
2024-03-05 11:33:09 +0300

    lb_2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -14,11 +14,48 @@ public class Calculator {
         return num1 * num2;
     }
 
-    public double divide(double num1, double num2) {
+    public int divide(int num1, int num2) {
         if (num2 == 0) {
             System.out.println("Деление на ноль невозможно.");
-            return Double.NaN;
         }
         return num1 / num2;
+    }
+    public Integer tem (String strok){
+        String[] parts = strok.split(" ");
+        // int a = Integer.parseInt(parts[0]);
+        // int b = Integer.parseInt(parts[2]);
+        int res= 0;
+        int a= 0;
+        int b= 0;
+        try {
+            a = Integer.parseInt(parts[0]);
+            b = Integer.parseInt(parts[2]);
+        }catch(NumberFormatException exception){
+            System.out.println("введено не число");
+        }
+        switch (parts[1]) {
+            case "+": {
+                res = add(a,b);
+                break;
+            }
+            case "-": {
+                res = subtract(a,b);
+                break;
+            }
+            case "*": {
+                res = multiply(a,b);
+                break;
+            }
+            case "/": {
+                // if(b==0) System.out.println("на 0 делить нельзя");
+                // else{
+                // res = a/b;
+                // mystring = Integer.toString(res);}
+                // break;
+                res = divide(a,b);
+                break;
+            }
+        }
+    return res;
     }
 }
--- src/main/java/app.java
+++ src/main/java/app.java
@@ -3,38 +3,41 @@ import java.util.Scanner;
 public class app {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        System.out.println("введите выражение:");
         Scanner in= new Scanner(System.in);
         String strok = in.nextLine();
-        String[] parts = strok.split(" ");
-        int a;
-        int b;
-        try {
-             a = Integer.parseInt(parts[0]);
-             b = Integer.parseInt(parts[2]);
-        }catch(NumberFormatException exception){
-            System.out.println("введено не число");
-            return;
-        }
-        switch (parts[1]) {
-            case "+": {
-                System.out.println(calculator.add(a, b));
-                break;
-            }
-            case "-": {
-                System.out.println(calculator.subtract(a, b));
-                break;
-            }
-            case "*": {
-                System.out.println(calculator.multiply(a, b));
-                break;
-            }
-            case "/": {
-                System.out.println(calculator.divide(a, b));
-                // if(b==0) System.out.println("на 0 делить нельзя");
-                // else System.out.println(a/b);
-                // break;
-            }
-        }
+        System.out.println("Результат:");
+        System.out.println(calculator.tem(strok));
+        // String[] parts = strok.split(" ");
+        // int a;
+        // int b;
+        // try {
+        //      a = Integer.parseInt(parts[0]);
+        //      b = Integer.parseInt(parts[2]);
+        // }catch(NumberFormatException exception){
+        //     System.out.println("введено не число");
+        //     return;
+        // }
+        // switch (parts[1]) {
+        //     case "+": {
+        //         System.out.println(calculator.add(a, b));
+        //         break;
+        //     }
+        //     case "-": {
+        //         System.out.println(calculator.subtract(a, b));
+        //         break;
+        //     }
+        //     case "*": {
+        //         System.out.println(calculator.multiply(a, b));
+        //         break;
+        //     }
+        //     case "/": {
+        //         System.out.println(calculator.divide(a, b));
+        //         // if(b==0) System.out.println("на 0 делить нельзя");
+        //         // else System.out.println(a/b);
+        //         // break;
+        //     }
+        // }
     }
 }
 // case "+": {
